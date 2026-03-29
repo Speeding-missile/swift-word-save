75e4defd3fbacb4d47b1ba7bc9248b9eabfe237b
Sree Vaishnav <vaeshnaev@gmail.com>
2026-03-29 20:57:20 +0530

    feat: add PWA manifest file for application metadata and icon configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.659168744dfe459e9f56101b94f4071b',
-  appName: 'WordVault',
+  appName: 'vocabulary(vault)',
   webDir: 'dist',
   server: {
     url: 'https://65916874-4dfe-459e-9f56-101b94f4071b.lovableproject.com?forceHideBadge=true',
